37e64e084608cf0ed97e6e504710c743033c146b
solstxce <48374053+solstxce@users.noreply.github.com>
2024-08-19 09:40:53 +0530

    initial OWASP KARE webpage

---
+++ owasp3.js
@@ -0,0 +1,4 @@
+// JavaScript to toggle the nav-links when the burger menu is clicked
+document.querySelector('.burger').addEventListener('click', () => {
+    document.querySelector('.nav-links').classList.toggle('active');
+});
